448dc52b512b949fd36505ddad93c6d17f84a993
Ewann <ewann.villa@gmail.com>
2021-07-14 21:02:24 +0200

    Adds Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: '279366841',
+});
 
 new Vue({
   vuetify,
